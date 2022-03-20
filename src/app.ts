89487175f2792f94f5cb82307794412683e1618c
William Ford <me@wford.dev>
2022-03-19 21:29:26 -0400

    Add redis cache

--- src/app.ts
+++ src/app.ts
@@ -1,24 +1,32 @@
 import * as Discord from 'discord.js';
 import 'dotenv/config';
 import InteractionHandler from './discord/InteractionHandler';
+import { CacheService } from './services/CacheService';
 import LoggerFactor from './services/Logging/LoggerFactory';
 import UfcService from './services/UfcService';
 import Environment from './util/Environment';
 
-const env = new Environment(process.env);
+const start = async () => {
+  const env = new Environment(process.env);
 
-const logger = LoggerFactor.createLogger(env);
-const dataService = new UfcService(logger);
-const interactionHandler = new InteractionHandler(logger, dataService);
+  const cacheService = new CacheService(env);
 
-const intents = [Discord.Intents.FLAGS.GUILDS];
+  await cacheService.init();
 
-const client = new Discord.Client({ intents });
+  const logger = LoggerFactor.createLogger(env);
+  const dataService = new UfcService(logger);
+  const interactionHandler = new InteractionHandler(logger, dataService, cacheService);
 
-client.once('ready', () => {
-  logger.info('Ready to accept commands');
-});
+  const intents = [Discord.Intents.FLAGS.GUILDS];
 
-client.on('interactionCreate', interactionHandler.handleInteraction);
+  const client = new Discord.Client({ intents });
 
-client.login(env.DISCORD_TOKEN);
+  client.once('ready', () => {
+    logger.info('Ready to accept commands');
+  });
+
+  client.on('interactionCreate', interactionHandler.handleInteraction);
+
+  client.login(env.DISCORD_TOKEN);
+}
+start();
---
+++ src/services/CacheService.ts
@@ -0,0 +1,30 @@
+import { RedisClientType } from '@node-redis/client';
+import redis = require('redis');
+import Environment from '../util/Environment';
+
+
+export class CacheService {
+  private readonly client: RedisClientType;
+
+  constructor(env: Environment) {
+    this.client = redis.createClient({url: env.REDIS_URL});
+  }
+
+  public init = async () => {
+    await this.client.connect();
+  }
+
+  public get = async (key: string) => {
+    return this.client.get(key);
+  }
+
+  public set = async (key: string, value: string) => {
+    return this.client.set(key, value);
+  }
+
+}
+
+
+
+
+
